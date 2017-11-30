38ef291c2d7608327b0e3cb7b397afea8312519a
lidev <605630907@qq.com>
2017-11-30 21:34:43 +0800

    first.

---
+++ app/src/main/java/com/example/myrepository/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myrepository;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hh", Toast.LENGTH_SHORT).show();
+    }
+}
